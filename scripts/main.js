aec00b614545e2152cd6200b2c5747e9fbd598b4
Dima <smirnov090502@gmail.com>
2022-03-19 17:16:11 +0600

    new

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,102 @@
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
+// setup canvas
 
-function setUserName() {
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
 }
 
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// function to generate random color
+
+function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-myButton.onclick = function () {
-    setUserName();
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
 }
 
-var myImage = document.querySelector('img');
+Ball.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
 
-myImage.onclick = function () {
-    var mySrc = myImage.getAttribute('src');
-    if (mySrc === 'https://i.pinimg.com/736x/70/5b/bb/705bbb820c7332b04d619f7536645753.jpg') {
-        myImage.setAttribute('src', 'https://klike.net/uploads/posts/2019-05/1556945414_2.jpg');
+Ball.prototype.update = function () {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
     }
-    else {
-        myImage.setAttribute('src', 'https://i.pinimg.com/736x/70/5b/bb/705bbb820c7332b04d619f7536645753.jpg');
+
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+}
+
+Ball.prototype.collisionDetect = function () {
+    for (var j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
     }
 }
+
+var balls = [];
+
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    while (balls.length < 25) {
+        var ball = new Ball(
+            random(0, width),
+            random(0, height),
+            random(-7, 7),
+            random(-7, 7),
+            'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+            random(10, 20)
+        );
+        balls.push(ball);
+    }
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+
+
+loop();
